1e5bf9fefa3a7794e67e803d65c4b633e34a20ee
renner <renner0@posteo.de>
2026-07-15 20:18:28 +0200

    exclude version numbers from os-release
    Makes the initramfs reproducible, smaller updates when kernel is
    unchanged, enables soft-reboot.
    
    Thanks secureblue

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,5 +10,16 @@ dnf do \
 
 mkdir -p /usr/lib/dracut/dracut.conf.d/
 printf 'reproducible=yes\nhostonly=no\ncompress=zstd\nadd_dracutmodules+=" bootc "' | tee "/usr/lib/dracut/dracut.conf.d/30-bootcrew-bootc-container-build.conf"
+
+# Temporarily patch /etc/os-release to avoid the initramfs depending on the
+# version number (which changes daily).
+tmp_release_file=$(mktemp --tmpdir 'os-release-XXXXXXXXXX')
+cp /etc/os-release "${tmp_release_file}"
+sed -Ei --follow-symlinks -e '/^(OSTREE_)?VERSION=/d' /etc/os-release
+
+export DRACUT_NO_XATTR=1
 dracut -v --force "$(find /usr/lib/modules -maxdepth 1 -type d | grep -v -E "*.img" | tail -n 1)/initramfs.img"
+
+cp "${tmp_release_file}" /etc/os-release
+rm "${tmp_release_file}"
 
