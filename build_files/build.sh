6c090a441f6952c517d0c970b51cd1e474b65276
renner <renner0@posteo.de>
2026-07-15 20:34:03 +0200

    no xattr

--- build_files/build.sh
+++ build_files/build.sh
@@ -17,6 +17,7 @@ tmp_release_file=$(mktemp --tmpdir 'os-release-XXXXXXXXXX')
 cp /usr/lib/os-release "${tmp_release_file}"
 sed -Ei -e '/^(OSTREE_)?VERSION=/d' /usr/lib/os-release
 
+export DRACUT_NO_XATTR=1
 dracut -v --force "$(find /usr/lib/modules -maxdepth 1 -type d | grep -v -E "*.img" | tail -n 1)/initramfs.img"
 
 cp "${tmp_release_file}" /usr/lib/os-release
