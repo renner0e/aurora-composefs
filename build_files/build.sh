03b77e685f5ae975f7f6e8a6d0479067084071df
renner <renner0@posteo.de>
2026-07-31 20:42:43 +0200

    feat: cleanup /{var,run,tmp}

--- build_files/build.sh
+++ build_files/build.sh
@@ -25,17 +25,4 @@ DRACUT_NO_XATTR=1 /usr/bin/dracut \
 cp "${TMP_OS_RELEASE}" /usr/lib/os-release
 rm "${TMP_OS_RELEASE}"
 
-# Relink rpm-ostree-base-db to rpmdb to ensure it correctly reflects the system
-# image's rpmdb and doesn't carry over package info from the base image.
-# See: https://github.com/coreos/rpm-ostree/issues/4554
-# https://forge.fedoraproject.org/atomic/tracker/issues/82
-for file in rpmdb.sqlite rpmdb.sqlite-shm rpmdb.sqlite-wal; do
-    target="/usr/share/rpm/${file}"
-    link_path="/usr/lib/sysimage/rpm-ostree-base-db/${file}"
-    if [[ -f "${target}" && -f "${link_path}" ]]; then
-        # Note, this needs to be a hardlink, not a symbolic link.
-        ln -f "${target}" "${link_path}"
-    fi
-done
-
-mv /etc/dnf/dnf.conf.bak /etc/dnf/dnf.conf
+/ctx/post.sh
